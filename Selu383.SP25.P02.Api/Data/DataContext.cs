6a4a6ab660836127ccac1731f412ce174338f023
nicksolari79 <nick.solari79@gmail.com>
2025-02-19 19:16:41 -0600

    Correct DataContext
    this datacontext passes the datacontext tests

--- Selu383.SP25.P02.Api/Data/DataContext.cs
+++ Selu383.SP25.P02.Api/Data/DataContext.cs
@@ -1,14 +1,46 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Selu383.SP25.P02.Api.Features.Theaters;
+using Selu383.SP25.P02.Api.Features.Users;
+using Selu383.SP25.P02.Api.Features.Roles;
+using Selu383.SP25.P02.Api.Features.UserRoles;
+using Microsoft.AspNetCore.Identity;
 
 namespace Selu383.SP25.P02.Api.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>,
+        UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        public DbSet<Theater> Theaters { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
         {
         }
 
-        public DbSet<Theater> Theaters { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Identity configurations
+            builder.Entity<User>()
+                .HasIndex(u => u.UserName)
+                .IsUnique();
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(u => u.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+        }
     }
 }
